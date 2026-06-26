44e1d9f4fefed416a88c4bf3d54eb3f9d0f2ccbf
danielgarciadasilva <daniel.garciadasilva3@gmail.com>
2026-06-25 21:40:44 -0300

    Exercicio

---
+++ src/main/java/exercicio/exercicio4.java
@@ -0,0 +1,27 @@
+package exercicio;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/produto/*")
+public class exercicio4 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+
+        String pathInfo = req.getPathInfo();
+
+        if (pathInfo != null && pathInfo.length() > 1) {
+            resp.getWriter().println("<h1> Você esta vendo os detalhes do produto: " + pathInfo.substring(1) + "</h1>");
+        } else {
+            resp.getWriter().println("<h1>Produto não informado</h1>");
+        }
+    }
+}
+
